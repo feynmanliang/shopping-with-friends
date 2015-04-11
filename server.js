a35020f459be0d9ba7da4eeef6cfadd0ba53c1df
Feynman Liang <feynman.liang@gmail.com>
2015-04-11 13:36:39 -0400

    Add react and shop lib

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var server = require('nodebootstrap-server');
 
 server.setup(function(runningApp) {
-  
+
   // runningApp.use(require('express-session')({secret: CONF.app.cookie_secret, resave: false, saveUninitialized: false}));
-  
+
   //---- Mounting well-encapsulated application modules
   //---- See: http://vimeo.com/56166857
 
   runningApp.use('/hello', require('hello')); // attach to sub-route
+  runningApp.use('/shop', require('shop')); // attach to sub-route
   runningApp.use(require('routes')); // attach to root route
-  
+
 });
