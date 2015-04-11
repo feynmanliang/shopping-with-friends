1e4e4e368eb0f96912f4fbddf8028b6b28c56546
Feynman Liang <feynman.liang@gmail.com>
2015-04-11 19:26:45 -0400

    Add socket.io

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var server = require('nodebootstrap-server');
 require('dotenv').load();
+var server = require('nodebootstrap-server');
 
-server.setup(function(runningApp) {
+server.setup(function(runningApp, http) {
 
   // runningApp.use(require('express-session')({secret: CONF.app.cookie_secret, resave: false, saveUninitialized: false}));
 
   //---- Mounting well-encapsulated application modules
   //---- See: http://vimeo.com/56166857
 
-  runningApp.use('/sms', require('sms')); // attach to sub-route
+  var io = require('socket.io')(http);
+
+  runningApp.use('/sms', require('sms')(io)); // attach to sub-route
   runningApp.use('/shop', require('shop')); // attach to sub-route
   runningApp.use(require('routes')); // attach to root route
 
