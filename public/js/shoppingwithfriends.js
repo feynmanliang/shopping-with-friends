fd4ecbb41af0d0651cb8de6a78da51be11f25c45
Feynman Liang <feynman.liang@gmail.com>
2015-04-11 21:37:42 -0400

    Broken code for multiple forms

--- public/js/shoppingwithfriends.js
+++ public/js/shoppingwithfriends.js
@@ -9,65 +9,80 @@ var ShopForm = React.createClass({
     this.setState( { } );
   },
   render: function() {
-    if (!this.state.name) {
       return (
         <div>
-          <div class="section no-pad-bot" id="index-banner">
-            <div class="container">
+          <div className="section no-pad-bot" id="index-banner">
+            <div className="container">
               <br /><br />
-              <h1 class="header center orange-text">Going to the store?</h1>
-              <div class="row center">
-                <h5 class="header col s12 light">Maybe your friends want something? You should let them know!</h5>
+              <h1 className="header center orange-text">Going to the store?</h1>
+              <div className="row center">
+                <h5 className="header col s12 light">Maybe your friends want something?</h5>
               </div>
               <br /><br />
             </div>
           </div>
-          <div class="section">
-            Where are you going?
-            <input type="text" ref="titlefield" placeholder="Store"/>
+          <div className="section">
+            <div className="row">
+                <input type="text" className="input" id="field1" placeholder="Name of Store" />
+                <label for="field1">Where are you going?</label>
 
-            Who should we notify?
-            <input type="text" ref="titlefield" placeholder="Friends"/>
+                <input type="text" className="input" id="field2" placeholder="Friends" />
+                <label for="field2">Who should we notify?</label>
+
+                <input type="hidden" name="count" value="1" />
+                <div className="control-group" id="fields">
+                    <label className="control-label" for="field1">Nice Multiple Form Fields</label>
+                    <div className="controls" id="profs">
+                        <form className="input-append">
+                            <div id="field">
+                              <input autocomplete="off" className="input" id="field1" name="prof1" type="text" placeholder="Type something" data-items="8"/>
+                              <button id="b1" className="btn add-more" type="button">+</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
             <br />
 
-            <button onClick={this._makeList}>Send SMS!</button>
+            <button className="btn waves-effect waves-light" onClick={this._makeList}>
+            <i className="mdi-content-send right"></i>
+            Let Them Know
+            </button>
           </div>
           <br /><br />
         </div>
-      )
-    } else {
-      var LIST = [];
-      if (typeof this.state.items !== 'undefined') {
-        console.log('currData.items');
-        console.log(this.state.items);
-        for (var i = 0; i < this.state.items.length; i++) {
-          LIST.push(<tr>
-                    <td>{this.state.items[i].owner}</td>
-                    <td>{this.state.items[i].item}</td>
-                    <td><input type="text" ref="pricefield" placeholder="enter price"/></td>
-                    <td><button onClick={this._handleDelete} id={this.state.items[i]._id}>destroy</button></td>
-                    </tr>);
-        }
-      }
-      return (
-        <div>
-          <input type="text" ref="namefield" placeholder="name.." />
-          <textarea ref="itemfield" rows="5" cols="30" placeholer="enter items separated by commas.." />
-          <button onClick={this._handleSubmit}>submit</button>
-          <table>
-            <thead>
-              <tr>
-                <th id="titleid"><h1>{this.state.name}</h1></th>
-              </tr>
-            </thead>
-            <tbody>
-              {LIST}
-            </tbody>
-          </table>
-        </div>
       );
-    }
+      //var LIST = [];
+      //if (typeof this.state.items !== 'undefined') {
+        //console.log('currData.items');
+        //console.log(this.state.items);
+        //for (var i = 0; i < this.state.items.length; i++) {
+          //LIST.push(<tr>
+                    //<td>{this.state.items[i].owner}</td>
+                    //<td>{this.state.items[i].item}</td>
+                    //<td><input type="text" ref="pricefield" placeholder="enter price"/></td>
+                    //<td><button onClick={this._handleDelete} id={this.state.items[i]._id}>destroy</button></td>
+                    //</tr>);
+        //}
+      //}
+      //return (
+        //<div>
+          //<input type="text" ref="namefield" placeholder="name.." />
+          //<textarea ref="itemfield" rows="5" cols="30" placeholer="enter items separated by commas.." />
+          //<button onClick={this._handleSubmit}>submit</button>
+          //<table>
+            //<thead>
+              //<tr>
+                //<th id="titleid"><h1>{this.state.name}</h1></th>
+              //</tr>
+            //</thead>
+            //<tbody>
+              //{LIST}
+            //</tbody>
+          //</table>
+        //</div>
+      //);
   },
   _makeList: function(e) {
     e.preventDefault();
